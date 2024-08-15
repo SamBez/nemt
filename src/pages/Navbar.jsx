0b0dd186103c69e847750478e54a363e5ebd256d
samrawit <gonesam77@gmail.com>
2024-08-14 23:35:44 -0400

    contact form

--- src/pages/Navbar.jsx
+++ src/pages/Navbar.jsx
@@ -36,7 +36,7 @@ function Navbar() {
         <div className={`w-full flex-grow lg:flex lg:items-center lg:w-auto ${isOpen ? 'block' : 'hidden'} mt-2 lg:mt-0 bg-white lg:bg-transparent text-black p-4 lg:p-0 z-20`} id="nav-content">
           <ul className="list-reset lg:flex justify-end flex-1 items-center">
             <li className="mr-3">
-              <a className="inline-block py-2 px-4 text-black font-bold no-underline" onClick={toggleMenu} href="#">Home</a>
+              <a className="inline-block py-2 px-4 text-black font-bold no-underline" onClick={toggleMenu} href="/">Home</a>
             </li>
             <li className="mr-3">
               <a className="inline-block text-black no-underline hover:text-gray-800 hover:text-underline py-2 px-4" onClick={toggleMenu} href="#about">About</a>
